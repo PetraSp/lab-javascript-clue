46270d34fab3e98d6e9a8d6c040f0d56c78e82b3
Petra Spirkova <pspirkova@gmail.com>
2017-09-19 22:07:09 +0200

    exercise done

--- starter-code/clue.js
+++ starter-code/clue.js
@@ -1,104 +1,93 @@
-/*
-
----- Characters ----
-
-  Mr Green
-  first name: Jacob
-  last name:  Green
-  color: green
-  description: He has a lot of connections
-  age: 45
-  image: https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
-  occupation: Entrepreneur
-
-  Dr Orchid
-  first name: Doctor
-  last name: Orchid
-  color: white
-  description: PhD in plant toxicology. Adopted daughter of Mr. Boddy
-  age: 26
-  image: http://www.radiotimes.com/uploads/images/Original/111967.jpg
-  occupation: Scientist
-
-  Prof Plum
-  first name: Victor
-  last name: Plum
-  color: purple
-  description: Billionare video game designer
-  age: 22
-  image: https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg
-  occupation: Designer
-
-  Miss Scarlet
-  first name: Kasandra
-  last name: Scarlet
-  color: red
-  description: She is an A-list movie star with a dark past,
-  age: 31,
-  image: https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg,
-  occupation: Actor
-
- Mrs Peacock
-  first name: Eleanor
-  last name: Peacock
-  color: blue
-  description: She is from a wealthy family and uses her status and money to earn popularity
-  age: 36
-  image: https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
-  occupation: Socialité
-
- Mr Mustard
-  first name: Jack
-  last name: Mustard
-  color: yellow
-  description: He is a former football player who tries to get by on his former glory
-  age: 62
-  image: https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg
-  occupation: Retired Football player
-
----- Weapons ----
-
-Rope - Weight: 10
-Knife - Weight: 8
-Candlestick - Weight: 2
-Dumbbell - Weight: 30
-Poison - Weight: 2
-Axe - Weight: 15
-Bat - Weight: 13
-Trophy - Weight: 25
-Pistol - Weight: 20
-
-
----- Rooms ----
-Dinning Room
-Conservatory
-Kitchen
-Study
-Library
-Billiard Room
-Lounge
-Ballroom
-Hall
-Spa
-Living Room
-Observatory
-Theater
-Guest House
-Patio
-
-*/
-
-function random_selector(array) {
-
-}
-
-function pick_mistery() {
-
-}
-
-function reveal_mistery(mistery) {
-
-}
-
-var mistery_envelope = pick_mistery();
-reveal_mistery(mistery_envelope);
+var game = {
+  suspects: [
+    {firstName: "Jacob",
+    lastName:  "Green",
+    color: "green",
+    description: "He has a lot of connections",
+    age: 45,
+    image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+    occupation: "Entrepreneur"},
+  
+    {firstName: "Doctor",
+    lastName: "Orchid",
+    color: "white",
+    description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+    age: 26,
+    image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    occupation: "Scientist"},
+  
+    {firstName: "Victor",
+    lastName: "Plum",
+    color: "purple",
+    description: "Billionare video game designer",
+    age: 22,
+    image: "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+    occupation: "Designer"},
+  
+    {firstName: "Kasandra",
+    lastName: "Scarlet",
+    color: "red",
+    description: "She is an A-list movie star with a dark past",
+    age: 31,
+    image: "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+    occupation: "Actor"},
+  
+    {firstName: "Eleanor",
+    lastName: "Peacock",
+    color: "blue",
+    description: "She is from a wealthy family and uses her status and money to earn popularity",
+    age: 36,
+    image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+    occupation: "Socialité"},
+    
+    {firstName: "Jack",
+    lastName: "Mustard",
+    color: "yellow",
+    description: "He is a former football player who tries to get by on his former glory",
+    age: 62,
+    image: "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+    occupation: "Retired Football player"},
+    ],
+  
+    weapons: [
+    {type: "rope", weight: 10},
+    {type: "knife", weight: 8},
+    {type: "candlestick", weight: 2},
+    {type: "Dumbbell", weight: 30},
+    {type: "Poison", weight: 2},
+    {type: "Axe", weight: 15},
+    {type: "Bat", weight: 13},
+    {type: "Trophy", weight: 25},
+    {type: "Pistol", weight: 20},
+    ],
+  
+    rooms: ["Dinning Room", "Conservatory", "Kitchen", "Study Library", "Billiard Room", "Lounge Ballroom", "Hall", "Spa", "Living Room", "Observatory", "Theater",
+    "Guest House", "Patio"],
+  
+    caseFileConfidential: {}
+    }
+  
+  
+  function random_selector(array) {
+    var randomCardIndex = Math.floor(Math.random() * array.length);
+    return array[randomCardIndex]
+  }
+  
+  
+  function pick_mistery() {
+    var randomlySelectedSuspect = random_selector(game.suspects);
+    var randomlySelectedWeapon = random_selector(game.weapons);
+    var randomlySelectedRoom = random_selector(game.rooms);
+    game.caseFileConfidential = { who: randomlySelectedSuspect.firstName, what: randomlySelectedWeapon.type, where: randomlySelectedRoom}     
+  }
+  
+  function reveal_mistery(mistery) {
+    console.log("The character is: " + game.caseFileConfidential.who)
+    console.log("The weapon is: " + game.caseFileConfidential.what)
+    console.log("The room is: " + game.caseFileConfidential.where)
+  }
+  
+  var mistery_envelope = pick_mistery();
+  reveal_mistery(mistery_envelope);
+  
+  
